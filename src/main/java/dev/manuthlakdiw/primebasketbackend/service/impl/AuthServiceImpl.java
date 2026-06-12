843cced9f1e1acd2d7420ffda3088c9918b50b57
L.K.H.M Lakdiw <manuthlakdiw2006@gmail.com>
2026-06-12 17:00:03 +0530

    Add OTP generation and Redis caching for user registration
    - Introduced OTP generation functionality with a `generateOtp` method.
    - Added Redis to cache OTPs with a 2-minute expiration during registration.
    - Integrated `spring-boot-starter-data-redis` dependency in `pom.xml`.

--- src/main/java/dev/manuthlakdiw/primebasketbackend/service/impl/AuthServiceImpl.java
+++ src/main/java/dev/manuthlakdiw/primebasketbackend/service/impl/AuthServiceImpl.java
@@ -4,12 +4,15 @@ import dev.manuthlakdiw.primebasketbackend.dto.auth.AuthResponse;
 import dev.manuthlakdiw.primebasketbackend.dto.auth.RegisterRequest;
 import dev.manuthlakdiw.primebasketbackend.entity.UserEntity;
 import dev.manuthlakdiw.primebasketbackend.entity.types.AuthProviderType;
-import dev.manuthlakdiw.primebasketbackend.entity.types.RoleType;
 import dev.manuthlakdiw.primebasketbackend.repository.UserRepository;
 import dev.manuthlakdiw.primebasketbackend.service.AuthService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author manuthlakdiv
@@ -23,6 +26,7 @@ import org.springframework.stereotype.Service;
 public class AuthServiceImpl implements AuthService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final StringRedisTemplate redisTemplate;
 
 
     @Override
@@ -32,6 +36,7 @@ public class AuthServiceImpl implements AuthService {
             throw new RuntimeException("Email is already in use");
         }
 
+        String otp = generateOtp();
 
         UserEntity user = UserEntity.builder()
                 .firstName(request.firstName())
@@ -43,6 +48,9 @@ public class AuthServiceImpl implements AuthService {
 
         UserEntity savedUser = userRepository.save(user);
 
+        String redisKey = "OTP:" + savedUser.getEmail();
+        redisTemplate.opsForValue().set(redisKey, otp, 2, TimeUnit.MINUTES);
+
         return AuthResponse.builder()
                 .email(savedUser.getEmail())
                 .firstName(savedUser.getFirstName())
@@ -50,4 +58,18 @@ public class AuthServiceImpl implements AuthService {
                 .role(savedUser.getRole().name())
                 .build();
     }
+
+
+    private String generateOtp() {
+        Random random = new Random();
+        int otpNumber = 10000 + random.nextInt(90000);
+        return String.valueOf(otpNumber);
+    }
+
+
+
 }
+
+
+
+
