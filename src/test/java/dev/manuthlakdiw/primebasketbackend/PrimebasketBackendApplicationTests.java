f76eeb76b78c77ac81bc673fc76a2e77d771ff1b
L.K.H.M Lakdiw <manuthlakdiw2006@gmail.com>
2026-06-27 17:16:54 +0530

    Add initial test class with context loading test

---
+++ src/test/java/dev/manuthlakdiw/primebasketbackend/PrimebasketBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.manuthlakdiw.primebasketbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimebasketBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
