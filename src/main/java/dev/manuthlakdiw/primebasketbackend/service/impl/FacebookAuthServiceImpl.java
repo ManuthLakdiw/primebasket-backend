0f0075f07717db98744f026da1402ba2cbec2770
L.K.H.M Lakdiw <manuthlakdiw2006@gmail.com>
2026-06-28 00:22:05 +0530

    Validate Facebook tokens using `debug_token` API to enhance security.

--- src/main/java/dev/manuthlakdiw/primebasketbackend/service/impl/FacebookAuthServiceImpl.java
+++ src/main/java/dev/manuthlakdiw/primebasketbackend/service/impl/FacebookAuthServiceImpl.java
@@ -1,6 +1,7 @@
 package dev.manuthlakdiw.primebasketbackend.service.impl;
 
 import dev.manuthlakdiw.primebasketbackend.service.FacebookAuthService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,15 +16,39 @@ import java.util.Map;
 
 @Service
 public class FacebookAuthServiceImpl implements FacebookAuthService {
+    @Value("${facebook.app.id}")
+    private String appId;
+
+    @Value("${facebook.app.secret}")
+    private String appSecret;
+
     @Override
     public Map<String, Object> verifyToken(String accessToken) {
+        RestTemplate restTemplate = new RestTemplate();
+        
         try {
-            RestTemplate restTemplate = new RestTemplate();
-            String url = "https://graph.facebook.com/me?fields=id,email,first_name,last_name&access_token=" + accessToken;
+            String debugTokenUrl = "https://graph.facebook.com/debug_token?input_token=" + accessToken + "&access_token=" + appId + "|" + appSecret;
+            Map<String, Object> debugResponse = restTemplate.getForObject(debugTokenUrl, Map.class);
 
+            if (debugResponse != null && debugResponse.containsKey("data")) {
+                Map<String, Object> data = (Map<String, Object>) debugResponse.get("data");
+
+                Boolean isValid = (Boolean) data.get("is_valid");
+
+                String tokenAppId = String.valueOf(data.get("app_id"));
+
+                if (isValid == null || !isValid || !appId.equals(tokenAppId)) {
+                    throw new RuntimeException("Security Breach: Invalid Facebook Token or App ID mismatch!");
+                }
+            } else {
+                throw new RuntimeException("Failed to validate Facebook token structure");
+            }
+
+            String url = "https://graph.facebook.com/me?fields=id,email,first_name,last_name&access_token=" + accessToken;
             return restTemplate.getForObject(url, Map.class);
+
         } catch (Exception e) {
-            throw new RuntimeException("Invalid Facebook Access Token");
+            throw new RuntimeException("Facebook Authentication Failed: " + e.getMessage());
         }
     }
 }
